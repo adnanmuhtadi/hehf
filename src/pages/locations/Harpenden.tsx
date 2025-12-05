d0c428a30e55f3dcf4e17311047edb2f8f4fdb65
Adnan Muhtadi <13140188+adnanmuhtadi@users.noreply.github.com>
2025-12-05 23:10:55 +0000

    harpenden

--- src/pages/locations/Harpenden.tsx
+++ src/pages/locations/Harpenden.tsx
@@ -1,7 +1,7 @@
 import LocationLayout from "@/layouts/LocationLayout";
 
 const harpendenImg = "https://i2-prod.mirror.co.uk/article32132399.ece/ALTERNATES/s1200/1_HarpendenHertfordshireengland-18032019AViewOfTheJunctionBetween.jpg";
-const harpendenMap = "https://www.google.com/maps/place/Harpenden/@51.8129244,-0.3621364,13z/data=!3m1!4b1!4m6!3m5!1s0x4876380b9f7e3c13:0x4eeb400a0ce7b472!8m2!3d51.81846!4d-0.358953!16zL20vMDF6Zzhr?entry=ttu&g_ep=EgoyMDI1MTIwMi4wIKXMDSoASAFQAw%3D%3D"; // use your map embed link
+const harpendenMap = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d39466.0770229886!2d-0.3621364!3d51.8129244!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4876380b9f7e3c13%3A0x4eeb400a0ce7b472!2sHarpenden!5e0!3m2!1sen!2suk!4v1764976131805!5m2!1sen!2suk"; // use your map embed link
 
 const Harpenden = () => (
   <LocationLayout
