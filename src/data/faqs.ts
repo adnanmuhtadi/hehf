23ae255b8c71818e3479d7ddf132408e444a7226
Adnan Muhtadi <13140188+adnanmuhtadi@users.noreply.github.com>
2025-12-05 17:06:51 +0000

    update on FAQ

--- src/data/faqs.ts
+++ src/data/faqs.ts
@@ -9,8 +9,11 @@ export const heroSubheading =
 
 export const faqs = [
     { q: "Do I need experience to become a host?", a: "No prior experience is needed—just a welcoming attitude and a willingness to help students settle in. We’ll guide you through every step." },
-    { q: "Can I host for just a few weeks?", a: "Absolutely! You can host for short stays, long stays, or even just one-off visits, depending on your availability." },
+    { q: "Can I host for just a few weeks?", a: "Absolutely! You can host for short stays, long stays, or even just one-off visits, depending on your availability and personal circumstances." },
     { q: "Will I have support if there’s a problem?", a: "Yes, our dedicated team is on hand 24/7 for advice, help, and practical support." },
+    { q: "What happens if there is a medical emergency?", a: "Unless it is a 999 call, each group will be provided a teacher/representative from HEHF who the first point of contact would be." },
     { q: "What about tax?", a: "You can earn up to £7,500 tax-free per year under the Rent-a-Room Scheme. We'll advise you on how it works." },
     { q: "How do I get started?", a: "Just fill in the enquiry form below or watch for our full application form coming soon!" },
+    { q: "How do I get started?", a: "It is preferred to have only have single beds or bunk beds. This would allow the host family to meet the criteria of both the agency and HEHF." },
+
   ];
